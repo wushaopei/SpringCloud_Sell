04e0cf575c20782f75c0b39b5fabbcca65b3f283
tom_glb <goolMorning_glb@webcode.com>
2019-11-15 18:38:19 +0800

    更新

---
+++ order/src/main/java/com/imooc/order/config/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.imooc.order.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName RestTemplateConfig
+ * @Description TODO
+ * @Author wushaopei
+ * @Date 2019/11/15 17:45
+ * @Version 1.0
+ */
+@Component
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}

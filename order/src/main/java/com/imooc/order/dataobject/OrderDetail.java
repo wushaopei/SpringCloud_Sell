2acfbf4760a4c989b15c5153bf1a27cdbd7db210
tom_glb <goolMorning_glb@webcode.com>
2019-11-15 11:23:38 +0800

    订单类、订单商品类

---
+++ order/src/main/java/com/imooc/order/dataobject/OrderDetail.java
@@ -0,0 +1,40 @@
+package com.imooc.order.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * @ClassName OrderDetail
+ * @Description TODO
+ * @Author wushaopei
+ * @Date 2019/11/15 11:20
+ * @Version 1.0
+ */
+@Data
+@Entity
+public class OrderDetail {
+
+    @Id
+    private String detailId;
+
+    /** 订单id. */
+    private String orderId;
+
+    /** 商品id. */
+    private String productId;
+
+    /** 商品名称. */
+    private String productName;
+
+    /** 商品单价. */
+    private BigDecimal productPrice;
+
+    /** 商品数量. */
+    private Integer productQuantity;
+
+    /** 商品小图. */
+    private String productIcon;
+}

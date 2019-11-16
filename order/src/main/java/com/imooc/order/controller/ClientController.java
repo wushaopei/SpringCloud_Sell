cea9bd9a1ce1c2fa3e70b65e22e2545005b85471
tom_glb <goolMorning_glb@webcode.com>
2019-11-16 12:36:48 +0800

    API文档说明

--- order/src/main/java/com/imooc/order/controller/ClientController.java
+++ order/src/main/java/com/imooc/order/controller/ClientController.java
@@ -28,14 +28,14 @@ public class ClientController {
     @GetMapping("/getProductMsg")
     public String getProductMsg(){
          //1.第一种方式 直接使用restTemplate,url写死
-//        RestTemplate restTemplate = new RestTemplate();
-//        String response  = restTemplate.getForObject("http://localhost:8772/msg", String.class);
+        RestTemplate restTemplate = new RestTemplate();
+        String response  = restTemplate.getForObject("http://localhost:8772/msg", String.class);
 
         // 2. 第二种方式 (利用loadBalancerClient通过应用名获取url,然后再使用restTemplate)
-        ServiceInstance serviceInstance = loadBalancerClient.choose("CLIENT");
-        String host = serviceInstance.getHost();
-        int port = serviceInstance.getPort();
-        log.info("{}-{}",host,port);
+//        ServiceInstance serviceInstance = loadBalancerClient.choose("CLIENT");
+//        String host = serviceInstance.getHost();
+//        int port = serviceInstance.getPort();
+//        log.info("{}-{}",host,port);
 //        String url = String.format("http://%s:%s", host, port+"/msg");
 //        RestTemplate restTemplate = new RestTemplate();
 //        String response  = restTemplate.getForObject(url, String.class);
@@ -44,7 +44,7 @@ public class ClientController {
 //        //3.第三种方式(利用@LoadBalanced,可在restTemplate里使用应用名字)
 //        String response = restTemplate.getForObject("http://PRODUCT/msg",String.class);
 //        log.info("response={}",response);
-//        return response;
-        return null;
+        return response;
+//        return null;
     }
 }
